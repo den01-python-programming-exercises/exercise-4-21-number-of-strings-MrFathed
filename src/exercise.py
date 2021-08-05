319aa6c425492427700a8ae5bb9aa51bffedd513
Justin Turner <jmturner638@gmail.com>
2021-08-05 12:04:25 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #write your code below this line
+    words = []
+
+    while True:
+        word = input("")
+
+        if not word:
+            break
+
+        words.append(word)
+
+    print(len(words))
 
 if __name__ == '__main__':
     main()
